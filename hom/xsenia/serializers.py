5834426f58c9d863d04167b70499608eb295fbe3
Michele Sorcinelli <michelesr@autistici.org>
2016-10-29 20:19:37 +0200

    Add address creation in Structure POST api

--- hom/xsenia/serializers.py
+++ hom/xsenia/serializers.py
@@ -1,9 +1,22 @@
-from .models import *
+from .models import Address, Structure
 from rest_framework import serializers
 
 
+class AddressSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Address
+        fields = ('text', 'phone', 'city', 'state', 'zip_code', 'country',
+                  'latitude', 'longitude')
+
+
 class StructureSerializer(serializers.HyperlinkedModelSerializer):
+    address = AddressSerializer()
     class Meta:
         model = Structure
-        fields = ('name', 'address', 'description', 'total_seats',
-                'available_seats', 'active')
+        fields = ('name', 'description', 'total_seats',
+                'available_seats', 'active', 'address')
+    def create(self, validated_data):
+        address_data = validated_data.pop('address')
+        address = Address.objects.create(**address_data)
+        structure = Structure.objects.create(address=address, **validated_data)
+        return structure
