ed067b9d60fbfdc7a97baae001e75d8da93ed79f
lucacesarini <lucacesarini001@gmail.com>
2016-10-30 01:42:59 +0200

    add dictionary creation

--- ui/ui/amm_main.js
+++ ui/ui/amm_main.js
@@ -7,18 +7,41 @@ app.controller('AmmMainController', [ '$scope', '$http',
 		$scope.evacueesData = [];
 
 		$scope.evacuessDict = {};
+		$scope.evacuessLabels = {};
+		$scope.evacuessValues = {};
 
 		var processEvacuees = function (evacuees) {
 			$scope.evacuessDict = {};
 			for (var i = 0; i < evacuees.length; i++) {
-			    evacuee = evacuees[i];
+			    var evacuee = evacuees[i];
 			    var cat = evacuee.category;
 			    if ($scope.evacuessDict.hasOwnProperty(cat)) {
 			    	$scope.evacuessDict[cat] = $scope.evacuessDict[cat] + 1;
 			    } else {
-			    	$scope.evacuessDict[cat] = 0;
+			    	$scope.evacuessDict[cat] = 1;
+			    }
+			    if (evacuees[i].group != null) {
+			    	for (var j = 0; j < evacuees[i].group.length; j++) {
+					    var evacuee = evacuees[i].group[j];
+					    var cat = evacuee.category;
+					    if ($scope.evacuessDict.hasOwnProperty(cat)) {
+					    	$scope.evacuessDict[cat] = $scope.evacuessDict[cat] + 1;
+					    } else {
+					    	$scope.evacuessDict[cat] = 1;
+					    }
+					}
 			    }
 			}
+			$scope.evacuessLabels = Object.keys($scope.evacuessDict);
+			$scope.evacuessValues = Object.values($scope.evacuessDict);
+
+			if ($scope.evacuessLabels[0] != null && $scope.evacuessValues[0] != null) {
+				xenia.bar.render("bar-chart", $scope.evacuessLabels, $scope.evacuessValues);
+				//xenia.bar.render("pie-chart", $scope.evacuessLabels, [2, 22, 8, 2, 1, 6]);
+			}
+
+			
+
 		}
 		
 		$scope.getStructures = function () {
@@ -41,13 +64,15 @@ app.controller('AmmMainController', [ '$scope', '$http',
 			};
 			$http(req).then(function(response) {
 				console.log(response);
-				$scope.evacueesData = response.data;
+				//$scope.evacueesData = response.data;
+				processEvacuees(response.data);
 			}, function(response) {
 				console.error(response.data);
 			});
 		}
 
 		$scope.getEvacuees();
+		$scope.getStructures();
 
 		
 	} ]);
