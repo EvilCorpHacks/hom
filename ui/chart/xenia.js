515ec18811e74cb560f3c878a7b203befc0b4aae
lucacesarini <lucacesarini001@gmail.com>
2016-10-29 17:07:59 +0200

    creazione libreria grafici xenia twitter snippet

---
+++ ui/chart/xenia.js
@@ -0,0 +1,75 @@
+var xenia = xenia || {};
+xenia.backgroundColors = [
+	    'rgba(255, 99, 132, 0.5)',
+	    'rgba(54, 162, 235, 0.5)',
+	    'rgba(255, 206, 86, 0.5)',
+	    'rgba(75, 192, 192, 0.5)',
+	    'rgba(153, 102, 255, 0.5)',
+	    'rgba(255, 159, 64, 0.5)'
+	];
+xenia.borderColors = [
+	    'rgba(255,99,132,1)',
+	    'rgba(54, 162, 235, 1)',
+	    'rgba(255, 206, 86, 1)',
+	    'rgba(75, 192, 192, 1)',
+	    'rgba(153, 102, 255, 1)',
+	    'rgba(255, 159, 64, 1)'
+	];
+xenia.pie = {
+	"render" : function (dom, labels, data) {
+		var ctx = document.getElementById(dom);
+		var myChart = new Chart(ctx, {
+		    type: 'pie',
+		    data: {
+		        labels: labels,
+		        datasets: [{
+		            label: '#',
+		            backgroundColor: xenia.backgroundColors,
+		        	//borderColor: xenia.borderColors,
+		            data: data,
+		            borderWidth: 1
+		        }]
+		    },
+		    options: {
+		        scales: {
+		            yAxes: [{
+		                ticks: {
+		                    beginAtZero:true
+		                }
+		            }]
+		        }
+		    }
+		});
+	}
+}
+xenia.bar = {
+	"render" : function (dom, labels, data) {
+		var ctx = document.getElementById(dom);
+		var myChart = new Chart(ctx, {
+		    type: 'bar',
+		    data: {
+		        labels: labels,
+		        datasets: [{
+		            label: '#',
+		            backgroundColor: xenia.backgroundColors,
+		        	//borderColor: xenia.borderColors,
+		            data: data,
+		            borderWidth: 1
+		        }]
+		    },
+		    options: {
+		        scales: {
+		            yAxes: [{
+		                ticks: {
+		                    beginAtZero:true
+		                }
+		            }]
+		        }
+		    }
+		});
+	}
+}
+
+
+
+
