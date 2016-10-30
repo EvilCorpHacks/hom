5aaaf3e57dc8099bf07c0ef33cf999842392378a
Michele Sorcinelli <michelesr@autistici.org>
2016-10-30 10:37:55 +0100

    MASS UI

--- ui/ui/amm_assign_1_structure.js
+++ ui/ui/amm_assign_1_structure.js
@@ -1,3 +1,9 @@
-app.controller('AmmAssign1StructureController', function($scope) {
-  $scope.greeting = 'Hola!';
+app.controller('AmmAssign1StructureController', function($rootScope, $scope, $http, $state) {
+  $http.get('/api/structures/').success(function(data) {
+    $scope.structures = data;
+  });
+  $scope.assignStructure = function(structure) {
+      $rootScope.structure = structure
+      $state.go('amm_assign_2_evacuees')
+  }
 });
--- ui/ui/amm_assign_2_evacuees.js
+++ ui/ui/amm_assign_2_evacuees.js
@@ -1,3 +1,20 @@
-app.controller('AmmAssign2EvacueesController', function($scope) {
-  $scope.greeting = 'Hola!';
+app.controller('AmmAssign2EvacueesController', function($rootScope, $state, $scope, $http) {
+   $scope.assignEvacuee = function(ev) {
+    $http.post('/api/assign', {'evacuee_id': ev.id, 'structure_id': $rootScope.structure.id})
+      .success(function() {
+         console.log('success');
+         $state.go('amm_assign_1_structure');
+      }).error(function() {
+         console.log('failed');
+         $state.go('amm_assign_1_structure');
+      });
+  };
+
+  $http.get('/api/evacuees').success(function(data) {
+    var evs;
+    $scope.evacuees = evs = data;
+    evs.forEach(function(ev) {
+      ev.group_size = ev.group.length + 1;
+    });
+  });
 });
