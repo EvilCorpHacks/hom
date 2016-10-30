7fac0e36ff8b5e907d7202dcd0080a52a13fdd85
Diego Magrini <soujiro@MacBook.local>
2016-10-30 09:48:45 +0100

    users queryset

--- ui/ui/vol_create_structure.js
+++ ui/ui/vol_create_structure.js
@@ -1,5 +1,5 @@
 app.controller('VolCreateStructureController', function($scope, $http) {
   $scope.submit = function(structure) {
     $http.post('/api/structures/', angular.copy(structure))
-  }
+  };
 });
--- ui/ui/vol_main.js
+++ ui/ui/vol_main.js
@@ -1,5 +1,9 @@
 app.controller('VolMainController', function($scope, $http) {
   $http.get('/api/structures/?user=1').success(function(data) {
-    $scope.structures = data
+    $scope.structures = data;
   });
+
+  $scope.new_structure = function(){
+    $
+  }
 });
